025466a427d6e6065f5d1066025bf2b4ff38d4af
Carlos Placencia <carlosplacencia2012@gmail.com>
2021-10-14 17:32:31 -0500

    CRUD routes set-up

--- controllers/useful_links.js
+++ controllers/useful_links.js
@@ -1,8 +1,72 @@
 const usefulLinksRouter = require('express').Router();
-const UsefulLinks = require('../models/useful_links');
+const UsefulLink = require('../models/useful_links');
 
+// get all links
 usefulLinksRouter.get('/', (req, res) => {
-  res.json({ message: 'Hello I am root route' });
+  UsefulLink.find({}).then((links) => {
+    res.json(links);
+  });
 });
 
+// get one link
+usefulLinksRouter.get('/:id', (req, res, next) => {
+  UsefulLink.findById(req.params.id).then((link) => {
+    if (link) {
+      res.json(link);
+    } else {
+      res.status(404).end();
+    }
+  })
+    .catch((error) => next(error));
+});
+
+// post one link
+usefulLinksRouter.post('/', (req, res, next) => {
+  const {
+    title, url, note, importance,
+  } = req.body;
+
+  const link = new UsefulLink({
+    title,
+    url,
+    note,
+    importance,
+  });
+
+  link.save().then((savedLink) => {
+    res.json(savedLink);
+  })
+    .catch((error) => next(error));
+});
+
+// delete one link
+usefulLinksRouter.delete('/:id', (req, res, next) => {
+  const linkId = req.params.id;
+
+  UsefulLink.findByIdAndRemove(linkId)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+// put one link
+usefulLinksRouter.put('/:id', (req, res, next) => {
+  const {
+    title, url, note, importance,
+  } = req.body;
+
+  const link = {
+    title,
+    url,
+    note,
+    importance,
+  };
+
+  UsefulLink.findByIdAndUpdate(req.params.id, link, { new: true, runValidators: true })
+    .then((updatedLink) => {
+      res.json(updatedLink);
+    })
+    .catch((error) => next(error));
+});
 module.exports = usefulLinksRouter;
